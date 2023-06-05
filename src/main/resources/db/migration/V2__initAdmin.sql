217b4feaf5d336e31ccb74e4746cc99a0d0a147b
Ma'ruf Raufov <116084220+dilshodivic@users.noreply.github.com>
2023-06-05 12:57:44 +0500

    Update V2__initAdmin.sql

--- src/main/resources/db/migration/V2__initAdmin.sql
+++ src/main/resources/db/migration/V2__initAdmin.sql
@@ -1,2 +1,2 @@
 insert into profile(name,surname,phone,password,role,status,created_date,visible,email)
-values ('Ali','Aliyev','0000','4a7d1ed414474e4033ac29ccb8653d9b','ROLE_ADMIN','ACTIVE','2022-12-22 23:28:35.46193',true,'raufovmaruf555@gmail.com')
+values ('Ali','Aliyev','0000','4a7d1ed414474e4033ac29ccb8653d9b','ROLE_ADMIN','ACTIVE','2022-12-22 23:28:35.46193',true,'ccccccccccc.com')
